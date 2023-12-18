a618ad70a71254fa0bac73edfdc671f392c199f4
limsss9076@naver.com <limsss9076@naver.com>
2023-12-18 13:35:52 +0900

    initial commit

---
+++ SwiftUIPractice/SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by 임승섭 on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
